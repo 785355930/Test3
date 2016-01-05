df976a3ca6b20bfbedf2aad2db344d1ebcadda86
Yin Dejin <785355930@qq.com>
2016-01-05 14:42:04 +0800

    Initial Commit

---
+++ Test3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test3
+//
+//  Created by 尹德金 on 16/1/5.
+//  Copyright © 2016年 zhifutong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Test3/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Test3
+//
+//  Created by 尹德金 on 16/1/5.
+//  Copyright © 2016年 zhifutong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
